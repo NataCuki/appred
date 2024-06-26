a74fb1460dc160dccca683e22cb141ccb4351bd3
Natalia Godoy <natgodoys@gmail.com>
2024-06-25 23:57:28 -0400

    'segundo'

--- lib/pages/first_page.dart
+++ lib/pages/first_page.dart
@@ -1,10 +1,41 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 import 'package:red/main.dart';
+import 'package:red/pages/gcp_services.dart';
 import 'package:red/pages/index_page.dart';
+import 'package:http/http.dart' as http;
 
-class FirstPage extends StatelessWidget {
+class FirstPage extends StatefulWidget {
   const FirstPage({super.key});
+
+  @override
+  State<FirstPage> createState() => _FirstPageState();
+}
+
+class _FirstPageState extends State<FirstPage> {
+  List<dynamic> _data = [];
+  GcpService _gcpService = GcpService();
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _fetchData();
+  }
+
+  Future<void> _fetchData() async {
+    final response = await http
+        .get(Uri.parse('https://app-gcp-api.onrender.com/bigquery-data'));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        _data = json.decode(response.body);
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,30 +67,39 @@ class FirstPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Container(
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 194, 25, 13),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Text('Horarios',
-                      style: TextStyle(color: Colors.white, fontSize: 30))),
-              const SizedBox(
-                height: 20,
+              // Container(
+              //     decoration: BoxDecoration(
+              //         color: const Color.fromARGB(255, 194, 25, 13),
+              //         borderRadius: BorderRadius.circular(10)),
+              //     child: const Text('Horarios',
+              //         style: TextStyle(color: Colors.white, fontSize: 30))),
+              // const SizedBox(
+              //   height: 20,
+              // ),
+              // Container(
+              //     decoration: BoxDecoration(
+              //         color: const Color.fromARGB(255, 194, 25, 13),
+              //         borderRadius: BorderRadius.circular(10)),
+              //     child: const Text('Recorridos',
+              //         style: TextStyle(color: Colors.white, fontSize: 30))),
+              // const SizedBox(
+              //   height: 20,
+              // ),
+              // Container(
+              //     decoration: BoxDecoration(
+              //         color: const Color.fromARGB(255, 194, 25, 13),
+              //         borderRadius: BorderRadius.circular(10)),
+              //     child: const Text('Paradas',
+              //         style: TextStyle(color: Colors.white, fontSize: 30))),
+              ListView.builder(
+                itemCount: _data.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    leading: Text('horario_tipo_dia'),
+                    title: Text(_data[index]['horario_tipo_dia'].toString()),
+                  );
+                },
               ),
-              Container(
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 194, 25, 13),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Text('Recorridos',
-                      style: TextStyle(color: Colors.white, fontSize: 30))),
-              const SizedBox(
-                height: 20,
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 194, 25, 13),
-                      borderRadius: BorderRadius.circular(10)),
-                  child: const Text('Paradas',
-                      style: TextStyle(color: Colors.white, fontSize: 30))),
             ],
           ),
         ),
